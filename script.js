001ee2cf31d89c6a917f8b6c5ab5842d5128dd78
wordora <wwordora@gmail.com>
2025-02-22 15:12:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function toggleMenu() {
-    let dropdown = document.getElementById("dropdownMenu");
-    dropdown.classList.toggle("show");
-}
 
-// Close menu when clicking outside
-document.addEventListener("click", function (event) {
-    let dropdown = document.getElementById("dropdownMenu");
-    let hamburger = document.querySelector(".hamburger");
-
-    if (!dropdown.contains(event.target) && !hamburger.contains(event.target)) {
-        dropdown.classList.remove("show");
-    }
-});
